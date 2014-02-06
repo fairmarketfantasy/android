2a2609db4a7e0b78a0c9a17b80d230cb47101b8d
bylynka <sergey.bylynka@easternpeak.com>
2014-02-06 12:24:08 +0200

    NBA-74 Get FB token

--- fantasysport/src/main/java/com/fantasysport/activities/MainActivity.java
+++ fantasysport/src/main/java/com/fantasysport/activities/MainActivity.java
@@ -1,9 +1,20 @@
 package com.fantasysport.activities;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Base64;
 import android.util.Log;
 import android.view.*;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.facebook.*;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
 import com.fantasysport.R;
 import com.fantasysport.models.User;
 import com.fantasysport.webaccess.RequestListeners.RequestError;
@@ -20,6 +31,8 @@ import com.octo.android.robospice.persistence.exception.SpiceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpClientErrorException;
 
+import java.security.MessageDigest;
+
 public class MainActivity extends BaseActivity {
 
     @Override
@@ -33,9 +46,31 @@ public class MainActivity extends BaseActivity {
         user.setRealName("my real name2");
         user.setPassword("123456");
         user.setPasswordConfirmation("123456");
-        testSignUp(user);
+//        testSignUp(user);
+
+        try{
+            PackageInfo info = getPackageManager().getPackageInfo("com.fantasysport",  PackageManager.GET_SIGNATURES);
+
+//            for (Signature signature : info.signatures)
+//            {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.d("------------KeyHash:-----------------------", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//            }
+        }catch (Exception e){}
+
+
+        LoginButton button =  (LoginButton)findViewById(R.id.facebookBtn);
+//        button.setLoginBehavior(SessionLoginBehavior.SUPPRESS_SSO);
+        button.setOnClickListener(_facebookBntListener);
     }
 
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+    }
 
     private void testSignUp(User user){
         SignUpRequest request = new SignUpRequest(user);
@@ -48,6 +83,15 @@ public class MainActivity extends BaseActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Session session = Session.getActiveSession();
+        if (session != null && session.isOpened()) {
+            Toast.makeText(this, session.getAccessToken(), Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
@@ -74,4 +118,24 @@ public class MainActivity extends BaseActivity {
         }
     };
 
+    View.OnClickListener _facebookBntListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            // start Facebook Login
+            Session.openActiveSession(MainActivity.this, true, new Session.StatusCallback() {
+
+                // callback when session changes state
+                @Override
+                public void call(Session session, SessionState state,
+                                 Exception exception) {
+
+                    if (session.isOpened()) {
+
+                    }
+                }
+            });
+
+        }
+    };
 }
