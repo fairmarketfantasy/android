19313a69c23f5d4aee8eaf31ab6d7d7fb765dc7c
bylynka <sergey.bylynka@easternpeak.com>
2014-04-17 11:30:05 +0300

    nba-670;

--- fantasysport/src/main/java/com/fantasysport/activities/BaseActivity.java
+++ fantasysport/src/main/java/com/fantasysport/activities/BaseActivity.java
@@ -37,7 +37,7 @@ public class BaseActivity extends ActionBarActivity{
     protected WebProxy _webProxy;
     protected Storage _storage;
     protected int _progressCounter;
-    private static final String APP_ID = "f7a14bcc9eb0186b4bf705f93229f693";
+    private static final String APP_ID = "3b749930a830362f2e47565e66fec8b8";
 
     @Override
     protected void onResume() {
